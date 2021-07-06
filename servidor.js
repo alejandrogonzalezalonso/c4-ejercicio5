efbb448852b60bd777d65f4609c73fda43182755
Alex <alejandro.gonzalez.7e3@itb.cat>
2021-07-06 10:47:49 +0200

    llamando a la api

--- servidor.js
+++ servidor.js
@@ -2,5 +2,29 @@ require ("dotenv").config();
 const fetch = require("node-fetch");
 const express = require("express");
 
+const { getLineasMetro } = require("./api");
+
+
+const app = express();
+
+
+const server = app.listen(4000, () => {
+  console.log("Servidor abierto");
+});
+
+server.on("error", (err) => {
+  if (err.code === "EADDRINUSE") {
+    console.log("No se ha podido levantar el servidor");
+  }
+});
+
+const getLineas = async()=>{
+  const resp = await fetch();
+  const { features } = await resp.json();
+  return features;
+}
+
+
+
 
 
